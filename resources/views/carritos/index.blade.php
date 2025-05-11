c9b011d0b6c25884e1c850eca909fecba5cf812f
kiddo <isaac.lopez0913@alumnos.udg.mx>
2025-05-11 17:58:35 -0600

    Actualización Paypal

--- resources/views/carritos/index.blade.php
+++ resources/views/carritos/index.blade.php
@@ -113,7 +113,7 @@
 
         <div class="flex justify-between items-center mt-5">
             <a href="{{ route('libros.index') }}" class="text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-500 dark:hover:bg-gray-600 dark:focus:ring-gray-800">Seguir Comprando</a>
-            <button class="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800">Finalizar Compra</button>
+            <a href="{{ route('paypal') }}" class="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800">Finalizar Compra</a>
         </div>
     @else
         <p class="text-center text-gray-500 dark:text-gray-400">Tu carrito está vacío.</p>
